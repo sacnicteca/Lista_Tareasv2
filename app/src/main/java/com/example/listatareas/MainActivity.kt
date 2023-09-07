83f53fada3b40356d7c59b0af99c4a40f1ced0a0
Sacnicté <202060588@ucc.mx>
2023-09-06 20:55:46 -0600

    Adelanto de la actividad

--- app/src/main/java/com/example/listatareas/MainActivity.kt
+++ app/src/main/java/com/example/listatareas/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.listatareas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lvTareas : ListView = findViewById(R.id.lvTareas)
+        val lista : MutableList<String> = mutableListOf()
+        lista.add("Lunes")
+        lista.add("Martes")
+        lista.add("Miércoles")
+        lista.add("Jueves")
+        lista.add("Viernes")
+        lista.add("Sábado")
+        lista.add("Domingo")
+
+        val adapter : ArrayAdapter<String> = ArrayAdapter(
+            applicationContext,
+            android.R.layout.simple_list_item_1,
+            lista
+        )
+
+        lvTareas.adapter = adapter
     }
 }
